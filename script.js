21344aeaf9ecb2728a72f8371ee62de9888204af
youngsoo-choi-scott <youngsu341@gmail.com>
2025-04-13 20:37:25 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const colors = ['#f6f6f6', '#e3e1e1', '#cfd8dc', '#fcefe6', '#fffde7'];
-let index = 0;
-
-window.changeBackground = () => {
-  index = (index + 1) % colors.length;
-  document.documentElement.style.setProperty('--bg-color', colors[index]);
-};
